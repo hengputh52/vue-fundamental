028c2b31ee2cb59dd4453ddbfa93278bcb9ca255
unknown <mheng770@gmail.com>
2026-04-28 13:52:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import Counter from './components/Counter.vue';
+export default {
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      message: "Not Found",
+      movies: [
+        {
+          title: "blade runner 2049",
+          cast: ["Ryan Gosling", "Charlie Puth"],
+          isFavorite: true,
+        },
+        {
+          title: "lala land",
+          cast: ["Ryan Gosling", "Emma Stone"],
+          isFavorite: false,
+        },
+        { title: "Notebook", cast: [], isFavorite: true },
+        { title: "Ben 10", cast: ["Charlie Puth"], isFavorite: false },
+        { title: "The Boys", cast: ["heng", "meng"], isFavorite: true },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <Counter />
+  <ul v-for="movie in movies">
+    <li v-if="movie.isFavorite">
+      {{ movie.title.toString() }}
+      <ul>
+        <li>
+          <p v-if="movie.cast.length > 0">{{ movie.cast.toString() }}</p>
+          <p v-else>{{ message }}</p>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data: () => ({
+    count: 0,
+    incrementAmount: 10,
+  }),
+  methods: {
+    changeIncrementAmount(event) {
+      this.incrementAmount = event.target.value;
+    },
+    incrementCounter() {
+      this.count += this.incrementAmount;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCounter">Increment Count</button>
+  <div>
+    <p>{{ incrementAmount }}</p>
+    <label>Increment by:</label>
+    <input
+      type="number"
+      v-bind:value="incrementAmount"
+      ,
+      v-on:input="changeIncrementAmount"
+    />
+  </div>
+</template>
