fe515bcf60a3a15bffe4f728051bf257a399c641
unknown <mheng770@gmail.com>
2026-05-09 22:02:25 +0700

    polling data in real time

--- bitcion-dashboard/src/components/bitcoin-card.vue
+++ bitcion-dashboard/src/components/bitcoin-card.vue
@@ -1,35 +1,47 @@
 <script setup>
-import { ref, watch, watchEffect } from 'vue';
+import { ref, watch, computed, onMounted } from 'vue';
 import { fetchData } from '../composables/useCoin'
 
+
+
 const reminderValue = ref()
-const reminderList = ref([])
-const { price } = await fetchData()
+const { price } =  fetchData()
 
-
-watch(reminderList.value, () => {
-    const lastIndex = reminderList.value.length - 1
-    const remind = reminderList.value[lastIndex]
-    if (Number(price.value.USD) >= remind) {
+ 
+watch(reminderValue, () => {
+    if (Number(price.value.USD) >= reminderValue.value) {
         console.log('reminder has been alerted')
     }
 
+})
 
-    
+const isTarget = computed(() => {
+    return Number(price.value.USD) >= reminderValue.value
 })
 
 function setReminder(value) {
-    reminderList.value.push(value)
+    reminderValue.value = value
+    console.log(reminderValue.value)
 }
 
-console.log(Number(price.value.USD))
+
+
+
+
 </script>
 
 <template>
 
-    <ul v-for="remind in reminderList" class="p-8 bg-emerald-400 rounded-xl mx-10 mb-5">
+    <!-- <ul v-for="remind in reminderList" class="p-8 bg-emerald-400 rounded-xl mx-10 mb-5">
         <li>{{ Number(price.USD) >= remind ? 'too high' : 'too low'}}</li>
-    </ul>
+    </ul> -->
+    <div :class="{ 'bg-emerald-400 p-10 mx-10 mb-10 rounded-xl': isTarget }">
+        <p class="text-center text-2xl text-amber-50 font-bold">{{ isTarget ? 'Price hit the target' : 'No alert message yet' }}</p>
+    </div>
+
+
+
+
 
     <div class="flex justify-around items-center p-10 bg-amber-300 mx-10 rounded-2xl">
         <div class="flex gap-4 text-2xl items-center">
@@ -50,10 +62,11 @@ console.log(Number(price.value.USD))
             <label class="font-bold text-2xl">Enter the price to set reminder</label>
         </div>
         <div class="flex gap-3">
-            <input v-model="reminderValue" type="number" class="border rounded-xl p-2" />
-            <button @click.prevent="setReminder(reminderValue)"
+            <input v-model="reminderValue" type="number" placeholder="enter the price" class="border rounded-xl p-2" />
+            <button @click="setReminder(reminderValue)"
                 class="border-none rounded-xl p-2 bg-blue-400 font-medium text-amber-50">Set Reminder</button>
         </div>
+
 
     </div>
 
--- bitcion-dashboard/src/composables/useCoin.js
+++ bitcion-dashboard/src/composables/useCoin.js
@@ -1,11 +1,23 @@
-import { ref } from 'vue'
-const price = ref([])
+import { ref, onMounted } from "vue";
 
+export function fetchData() {
+  const price = ref([]);
+  let timer = null;
+  const fetchPrice = async () => {
+    try {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR",
+      ).then((response) => response.json());
 
-export async function fetchData() {
-    price.value = await fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR').then((response) => response.json())
+      price.value = response;
+    } catch (error) {
+      console.log("failed to fetch data", error);
+    }
+  };
 
-    return {price}
+  onMounted(() => {
+    fetchPrice();
+    timer = setInterval(fetchPrice, 3000);
+  });
+  return { price };
 }
-
-
